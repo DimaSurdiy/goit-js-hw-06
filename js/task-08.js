d995624633f2810e90ab6a1f1851bb1760419250
DimaSurdiy <dsurdiy@ukr.net>
2022-01-13 21:01:04 +0100

    adds completed tasks 08 & 09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Все поля должны быть заполнены :)');
+  }
+
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtnEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = document.body.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
